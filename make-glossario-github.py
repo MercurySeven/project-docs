80901fa185c67231e42d17ba3a9218570c5c58f8
Davide Albiero <davide2910@live.it>
2021-01-06 14:03:30 +0100

    Fix CI

--- make-glossario-github.py
+++ make-glossario-github.py
@@ -43,10 +43,8 @@ def main() -> None:
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
         "token_uri": "https://oauth2.googleapis.com/token",
         "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-lxanj%40glossario-765f4.iam.gserviceaccount.com"
+        "client_x509_cert_url": os.environ.get('client_x509_cert_url')
     }
-
-    
 
     cred = credentials.Certificate(secrets)
 
